e6cf933eb9e65c3d55104ce1259453c34a05e27c
feverbrew <feverbrew@gmail.com>
2021-04-16 10:56:38 -0700

    slight modifications to initial react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  const name = 'Lucas'
+
+  return (
+    <div className="container">
+      <h1>Hello from React</h1>
+      <h2>Hello {name}</h2>
+    </div>
+  );
+}
+
+export default App;
