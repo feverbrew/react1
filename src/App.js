f8817816fb84abd99fbdb09edcc590a16861c386
feverbrew <feverbrew@gmail.com>
2021-04-17 13:52:44 -0700

    added Tasks and 'x button' functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import Header from "./components/Header"
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appt',
+      day: 'May 6th',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Hang out with friends',
+      day: 'April 17th',
+      reminder: false,
+    },
+  ])
+
+  //delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+    console.log('delete', id)
+  }
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} 
+      onDelete={deleteTask} /> : 'No Tasks Left'}
     </div>
   );
 }
