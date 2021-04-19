8acd9b98c182acbc6bfbe1e13dfabdcfecfe3877
feverbrew <feverbrew@gmail.com>
2021-04-18 17:15:39 -0700

    created the add task component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
+import AddTask from './components/AddTask'
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -18,17 +19,31 @@ function App() {
     },
   ])
 
+  //add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   //delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
     console.log('delete', id)
   }
 
+  //toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id
+    ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? <Tasks tasks={tasks} 
-      onDelete={deleteTask} /> : 'No Tasks Left'}
+      onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks Left'}
     </div>
   );
 }
